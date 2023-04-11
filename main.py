a4be3491eabccd671a55b978f12b87f67e667963
Library of Celsus <127127527+libraryofcelsus@users.noreply.github.com>
2023-04-11 16:55:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,30 +11,6 @@ def open_file(filepath):
         return infile.read()
 
 
-def main_menu_old():
-    print("***********************************")
-    print("*Welcome to the Aetherius Chatbot!*")
-    print("***********************************")
-    print("*Type [Exit] after making a choice*")
-    print("*    to return to this screen!    *")
-    print("***********************************")
-    script_dir = 'Scripts'
-    py_files = [f for f in os.listdir(script_dir) if f.endswith('.py')]
-    for i, py_file in enumerate(py_files):
-        filename = os.path.splitext(py_file)[0].replace('_', ' ')
-        print(f"{i+1}. {filename}")
-    choice = input("Please enter a number: ")
-    if choice.isdigit() and int(choice) in range(1, len(py_files)+1):
-        module_name = os.path.splitext(py_files[int(choice)-1])[0]
-        module = importlib.import_module(module_name)
-        functions = [func for func in dir(module) if callable(getattr(module, func))]
-        for function_name in functions:
-            getattr(module, function_name)()
-    else:
-        print("Invalid choice. Please try again.")
-        main_menu()
-
-
 def main_menu():
     print("***********************************")
     print("*Welcome to the Aetherius Main Menu*")

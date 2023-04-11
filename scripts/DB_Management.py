276b63eaaa4443b5e56e7c47ef9b2ebd63f544c4
Library of Celsus <127127527+libraryofcelsus@users.noreply.github.com>
2023-04-11 10:10:01 -0700

    Add files via upload

--- scripts/DB_Management.py
+++ scripts/DB_Management.py
@@ -12,23 +12,32 @@ def DB_Management():
     print("*    Welcome to  the Aetherius    *")
     print("*         Database Manager        *")
     print("***********************************")
-    print("*Type [Exit] after making a choice*")
-    print("*    to return to this screen!    *")
+    print("*  Type [Exit] to return to the   *")
+    print("*           main menu!            *")
     print("***********************************")
-    script_dir = './scripts/DB_Management'
-    py_files = [f for f in os.listdir(script_dir) if f.endswith('.py')]
-    for i, py_file in enumerate(py_files):
-        filename = os.path.splitext(py_file)[0].replace('_', ' ')
-        print(f"{i+1}. {filename}")
-    choice = input("Please enter a number: ")
-    if choice == 'Exit':
-        return
-    if choice.isdigit() and int(choice) in range(1, len(py_files)+1):
-        module_name = os.path.splitext(py_files[int(choice)-1])[0]
-        module = importlib.import_module(module_name)
-        functions = [func for func in dir(module) if callable(getattr(module, func))]
-        for function_name in functions:
-            getattr(module, function_name)()
+    # Get a list of all the files in the folder
+    files = os.listdir('scripts/DB_Management')
+    # Filter out only the .py files 
+    scripts = [file for file in files if file.endswith('.py')]
+    # Print the menu options
+    for i, script in enumerate(scripts):
+        # Replace underscores with spaces
+        script_name = script[:-3].replace('_', ' ')
+        print(f"{i+1}. {script_name}")
+    # Get the user's choice
+    choice = int(input("Enter your choice: "))
+    # Make sure the choice is valid
+    if 1 <= choice <= len(scripts):
+        # Get the chosen script
+        script = scripts[choice-1]
+        # Remove the .py extension to get the module name
+        module_name = script[:-3]
+        # Import the module
+        spec = importlib.util.spec_from_file_location(module_name, f"scripts/DB_Management/{script}")
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        # Call the function with the same name as the module
+        function = getattr(module, module_name)
+        function()
     else:
-        print("Invalid choice. Please try again.")
-        Main_Bot()
+        print("Invalid choice")

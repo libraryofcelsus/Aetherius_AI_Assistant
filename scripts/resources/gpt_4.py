acc2d6d0da942d40948a6dc97707f14ad377cddc
Library of Celsus <127127527+libraryofcelsus@users.noreply.github.com>
2023-05-08 09:56:48 -0700

    Add files via upload

--- scripts/resources/gpt_4.py
+++ scripts/resources/gpt_4.py
@@ -143,6 +143,24 @@ def chatgptresponse_completion(messages, model="gpt-4", temp=0.5):
             sleep(2 ** (retry - 1) * 5)
             
             
+def chatgptauto_completion(messages, model="gpt-4", temp=0.35):
+    max_retry = 7
+    retry = 0
+    while  True:
+        try:
+            response = openai.ChatCompletion.create(model=model, messages=messages)
+            text = response['choices'][0]['message']['content']
+            temperature = temp
+            return text
+        except Exception as oops:
+            retry += 1
+            if retry >= max_retry:
+                print(f"Exiting due to an error in ChatGPT: {oops}")
+                exit(1)
+            print(f'Error communicating with OpenAI: "{oops}" - Retrying in {2 ** (retry - 1) * 5} seconds...')
+            sleep(2 ** (retry - 1) * 5)
+            
+            
 def chatgptsummary_completion(messages, model="gpt-3.5-turbo", temp=0.1):
     max_retry = 5
     retry = 0

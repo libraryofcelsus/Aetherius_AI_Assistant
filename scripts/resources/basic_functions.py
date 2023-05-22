6979c7f4f1919f1f173890fa9b7e222d2872751c
Library of Celsus <127127527+libraryofcelsus@users.noreply.github.com>
2023-05-22 11:56:03 -0700

    Add files via upload

---
+++ scripts/resources/basic_functions.py
@@ -0,0 +1,137 @@
+import sys
+sys.path.insert(0, './scripts')
+sys.path.insert(0, './config')
+sys.path.insert(0, './config/Chatbot_Prompts')
+sys.path.insert(0, './scripts/resources')
+import os
+import openai
+import json
+import time
+from time import time, sleep
+import datetime
+from uuid import uuid4
+import pinecone
+
+
+def open_file(filepath):
+    with open(filepath, 'r', encoding='utf-8') as infile:
+        return infile.read()
+        
+        
+def save_file(filepath, content):
+    with open(filepath, 'w', encoding='utf-8') as outfile:
+        outfile.write(content)
+        
+        
+def load_json(filepath):
+    with open(filepath, 'r', encoding='utf-8') as infile:
+        return json.load(infile)
+        
+        
+def save_json(filepath, payload):
+    with open(filepath, 'w', encoding='utf-8') as outfile:
+        json.dump(payload, outfile, ensure_ascii=False, sort_keys=True, indent=2)
+        
+        
+def timestamp_to_datetime(unix_time):
+    return datetime.datetime.fromtimestamp(unix_time).strftime("%A, %B %d, %Y at %I:%M%p %Z")
+    
+    
+def is_integer(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+        
+                         
+def load_conversation_explicit_short_term_memory(results):
+    result = list()
+    for m in results['matches']:
+        info = load_json('nexus/explicit_short_term_memory_nexus/%s.json' % m['id'])
+        result.append(info)
+    ordered = sorted(result, key=lambda d: d['time'], reverse=False)  # sort them all chronologically
+    messages = [i['message'] for i in ordered]
+    return '\n'.join(messages).strip()
+    
+    
+def load_conversation_explicit_long_term_memory(results):
+    result = list()
+    for m in results['matches']:
+        info = load_json('nexus/explicit_long_term_memory_nexus/%s.json' % m['id'])
+        result.append(info)
+    ordered = sorted(result, key=lambda d: d['time'], reverse=False)  # sort them all chronologically
+    messages = [i['message'] for i in ordered]
+    return '\n'.join(messages).strip()
+    
+    
+def load_conversation_episodic_memory(results):
+    result = list()
+    for m in results['matches']:
+        info = load_json('nexus/episodic_memory_nexus/%s.json' % m['id'])
+        result.append(info)
+    ordered = sorted(result, key=lambda d: d['time'], reverse=False)  # sort them all chronologically
+    messages = [i['message'] for i in ordered]
+    return '\n'.join(messages).strip()  
+    
+    
+def load_conversation_flashbulb_memory(results):
+    result = list()
+    for m in results['matches']:
+        info = load_json('nexus/flashbulb_memory_nexus/%s.json' % m['id'])
+        result.append(info)
+    ordered = sorted(result, key=lambda d: d['time'], reverse=False)  # sort them all chronologically
+    messages = [i['message'] for i in ordered]
+    return '\n'.join(messages).strip() 
+    
+    
+def load_conversation_heuristics(results):
+    result = list()
+    for m in results['matches']:
+        info = load_json('nexus/heuristics_nexus/%s.json' % m['id'])
+        result.append(info)
+    ordered = sorted(result, key=lambda d: d['time'], reverse=False)  # sort them all chronologically
+    messages = [i['message'] for i in ordered]
+    return '\n'.join(messages).strip()    
+
+
+def load_conversation_implicit_short_term_memory(results):
+    result = list()
+    for m in results['matches']:
+        info = load_json('nexus/implicit_short_term_memory_nexus/%s.json' % m['id'])
+        result.append(info)
+    ordered = sorted(result, key=lambda d: d['time'], reverse=False)  # sort them all chronologically
+    messages = [i['message'] for i in ordered]
+    return '\n'.join(messages).strip()  
+    
+    
+def load_conversation_cadence(results):
+    result = list()
+    for m in results['matches']:
+        info = load_json('nexus/cadence_nexus/%s.json' % m['id'])
+        result.append(info)
+    ordered = sorted(result, key=lambda d: d['time'], reverse=False)  # sort them all chronologically
+    messages = [i['message'] for i in ordered]
+    return '\n'.join(messages).strip() 
+    
+    
+def load_conversation_implicit_long_term_memory(results):
+    result = list()
+    for m in results['matches']:
+        info = load_json('nexus/implicit_long_term_memory_nexus/%s.json' % m['id'])
+        result.append(info)
+    ordered = sorted(result, key=lambda d: d['time'], reverse=False)  # sort them all chronologically
+    messages = [i['message'] for i in ordered]
+    return '\n'.join(messages).strip()
+    
+def timeout_check():
+    try:
+        pinecone.init(api_key=open_file('api_keys/key_pinecone.txt'), environment=open_file('api_keys/key_pinecone_env.txt'))
+        vdb = pinecone.Index("aetherius")
+        return vdb
+    except pinecone.exceptions.PineconeException as e:
+        if "timed out" in str(e):
+            print("Connection timed out. Reconnecting...")
+            timeout_check()
+        else:
+            raise e

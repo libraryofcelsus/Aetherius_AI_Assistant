3441ee946716dfdf69b0f116c5d94311ac7d5237
Library of Celsus <127127527+libraryofcelsus@users.noreply.github.com>
2023-08-16 15:57:51 -0700

    Add files via upload

--- scripts/resources/install.bat
+++ scripts/resources/install.bat
@@ -1,4 +1,5 @@
 @echo off
+cd /d "%~dp0"
 echo Cloning the repository...
 git clone https://github.com/libraryofcelsus/Aetherius_AI_Assistant.git
 cd /d "%~dp0Aetherius_AI_Assistant"
@@ -7,14 +8,13 @@ echo Installing Python if not already installed...
 powershell -Command "if (-not (Get-Command python -ErrorAction SilentlyContinue)) {Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.10.6/python-3.10.6-amd64.exe' -OutFile 'python-installer.exe'; Start-Process 'python-installer.exe' -ArgumentList '/quiet InstallAllUsers=1 PrependPath=1' -Wait; Remove-Item 'python-installer.exe'; }"
 
 echo Creating a virtual environment...
-python -m venv venv
-call venv\Scripts\activate
+python -m venv "%~dp0Aetherius_AI_Assistant\venv"
 
 echo Installing project dependencies...
-pip3 install -r requirements.txt
+venv\Scripts\python -m pip install -r requirements.txt
 
 echo Running the project...
-python main.py
+"%~dp0Aetherius_AI_Assistant\venv\Scripts\python.exe" main.py
 
 echo Press any key to exit...
 pause >nul

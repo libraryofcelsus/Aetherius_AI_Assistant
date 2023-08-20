484f6721cff48c68b3d8cc237e6fd9b07bba0e99
Library of Celsus <127127527+libraryofcelsus@users.noreply.github.com>
2023-08-19 22:37:33 -0700

    Add files via upload

--- scripts/resources/install.bat
+++ scripts/resources/install.bat
@@ -18,72 +18,64 @@ if %errorlevel% equ 0 (
 
 
 
+setlocal enabledelayedexpansion
+
 cd /d "%~dp0"
+
+:: Uncomment this if you want to delete existing folder before cloning
+:: rmdir /s /q "Aetherius_AI_Assistant"
+
 echo Cloning the repository...
-git clone https://github.com/libraryofcelsus/Aetherius_AI_Assistant.git
-cd /d "%~dp0Aetherius_AI_Assistant"
+git clone https://github.com/libraryofcelsus/Aetherius_AI_Assistant.git || goto :error
+cd Aetherius_AI_Assistant
 
-
-setlocal
+:: Initialize PYTHON_PATH as empty
+set PYTHON_PATH=
 
 :: Manually specify paths to ignore
 set IGNORE_PATH=C:\Users\%USERNAME%\AppData\Local\Microsoft\WindowsApps\python.exe
 
 echo Checking known locations...
-:: Automatically find Python path in known locations
 for /f "delims=" %%i in ('where python 2^>nul') do (
     echo Found Python at: %%i
-    if not "%%i"=="%IGNORE_PATH%" (
+    if not "%%i"=="!IGNORE_PATH!" (
         set PYTHON_PATH=%%i
-        echo Set PYTHON_PATH to %%i
-        goto :foundPython
+        echo Set PYTHON_PATH to !PYTHON_PATH!
     ) else (
         echo Ignoring Python at: %%i
     )
 )
 
-echo Checking AppData...
-:: If not found, then check AppData
+:: Check if PYTHON_PATH was set
 if not defined PYTHON_PATH (
-    for %%X in (310 39 38 37 36) do (
-        if exist "%APPDATA%\Python\Python%%X\python.exe" (
-            echo Found Python in AppData at: %APPDATA%\Python\Python%%X\python.exe
-            set PYTHON_PATH=%APPDATA%\Python\Python%%X\python.exe
-            echo Set PYTHON_PATH to %APPDATA%\Python\Python%%X\python.exe
-            goto :foundPython
-        )
-    )
-)
-
-:: If Python was found in any of the locations
-:foundPython
-if defined PYTHON_PATH (
-    echo Python is already installed at %PYTHON_PATH%.
-    goto :end
-) else (
+    echo No acceptable Python installation found.
     echo Installing Python...
     powershell -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.10.6/python-3.10.6-amd64.exe' -OutFile 'python-installer.exe'; Start-Process 'python-installer.exe' -ArgumentList '/quiet InstallAllUsers=1 PrependPath=1 TargetDir=%APPDATA%\Python\Python310' -Wait; Remove-Item 'python-installer.exe';"
-    if errorlevel 1 (
-        echo Failed to install Python.
-        goto :end
-    )
-    echo Python installed at %APPDATA%\Python\Python310\python.exe
     set PYTHON_PATH=%APPDATA%\Python\Python310\python.exe
-    echo Set PYTHON_PATH to %APPDATA%\Python\Python310\python.exe
+    echo Set PYTHON_PATH to !PYTHON_PATH!
 )
 
-:end
-endlocal
+:: In case Python is not found or installed, terminate the script
+if not defined PYTHON_PATH (
+    echo Python was not found or installed. Exiting...
+    goto :error
+)
 
+echo Python is already installed at !PYTHON_PATH!.
 
-echo Creating a virtual environment...
-python -m venv "%~dp0Aetherius_AI_Assistant\venv"
+:: Create a virtual environment
+"!PYTHON_PATH!" -m venv "venv"
 
-echo Installing project dependencies...
-venv\Scripts\python -m pip install -r requirements.txt
+:: Install project dependencies
+"venv\Scripts\python" -m pip install -r requirements.txt
 
-echo Running the project...
-"%~dp0Aetherius_AI_Assistant\venv\Scripts\python.exe" main.py
+:: Run the project
+"venv\Scripts\python" main.py
 
 echo Press any key to exit...
+pause >nul
+goto :EOF
+
+:error
+echo An error occurred. Exiting...
 pause >nul

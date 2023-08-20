0a0181681719b93de579b843b72e6e74e4706ee3
Library of Celsus <127127527+libraryofcelsus@users.noreply.github.com>
2023-08-19 21:30:52 -0700

    Add files via upload

--- scripts/resources/install.bat
+++ scripts/resources/install.bat
@@ -1,4 +1,23 @@
 @echo off
+:: Check if Git is already installed
+where git >nul 2>nul
+if %errorlevel% equ 0 (
+    echo Git is already installed.
+) else (
+    :: Download Git installer
+    echo Downloading Git installer...
+    powershell -Command "Invoke-WebRequest -Uri 'https://github.com/git-for-windows/git/releases/download/v2.41.0.windows.3/Git-2.41.0.3-64-bit.exe' -OutFile '%TEMP%\GitInstaller.exe'"
+
+    :: Install Git
+    echo Installing Git...
+    %TEMP%\GitInstaller.exe /SILENT /COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh"
+
+    :: Delete the installer
+    del %TEMP%\GitInstaller.exe
+)
+
+
+
 cd /d "%~dp0"
 echo Cloning the repository...
 git clone https://github.com/libraryofcelsus/Aetherius_AI_Assistant.git

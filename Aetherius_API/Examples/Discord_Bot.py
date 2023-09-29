34130ea722201f4b9ce02d51cb93ca02f756f707
Library of Celsus <127127527+libraryofcelsus@users.noreply.github.com>
2023-09-28 19:20:07 -0700

    Add files via upload

--- Aetherius_API/Examples/Discord_Bot.py
+++ Aetherius_API/Examples/Discord_Bot.py
@@ -3,10 +3,10 @@ import os
 from discord.ext import commands
 from Aetherius_API.Oobabooga_Import import Aetherius_Chatbot
 import logging
+import concurrent.futures
+import asyncio
 
-# To use this script, move it to the main Aetherius Root Folder
-
-TOKEN = 'ENTER DISCORD BOT TOKEN HERE'
+TOKEN = 'REPLACE WITH DISCORD BOT TOKEN'
 
 intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
@@ -14,14 +14,20 @@ client = discord.Client(command_prefix='!', intents=intents)
 logger = logging.getLogger('discord')
 logger.setLevel(logging.ERROR)
 
+def chatbot_response(message):
+    return Aetherius_Chatbot(message.content, str(message.author), 'Aetherius')
+
 @client.event
 async def on_message(message):
     # Only respond to messages from other users, not from the bot itself
     if message.author == client.user:
         return
 
-    # Use the Aetherius_Chatbot function to generate a response to the message
-    response = Aetherius_Chatbot(message.content, str(message.author), 'Aetherius')
+    # Execute the chatbot_response function in a separate thread
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        response = await asyncio.get_event_loop().run_in_executor(
+            executor, chatbot_response, message
+        )
     
     # Send the response as a message
     await message.channel.send(response)

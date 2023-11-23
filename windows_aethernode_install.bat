f0a1a152a2cf049ba46fc40077aab7d378961686
Library of Celsus <127127527+libraryofcelsus@users.noreply.github.com>
2023-11-23 10:53:28 -0800

    Set AetherNode version to 0.03

--- windows_aethernode_install.bat
+++ windows_aethernode_install.bat
@@ -2,7 +2,7 @@
 REM Change directory to the script's location
 cd %~dp0
 
-git clone https://github.com/libraryofcelsus/AetherNode
+git clone https://github.com/libraryofcelsus/AetherNode --branch 0.03
 
 cd AetherNode
 
